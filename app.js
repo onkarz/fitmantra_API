d50dc58876ac218f459bc5ca3a3ae514072730d0
onkarzende <onkarzende1997@gmail.com>
2023-12-20 16:55:11 +0530

    Cors changes

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const postRouter = require("./routes/post");
 const dataSetRouter = require("./routes/dataSet");
 require("dotenv").config();
 
+app.use(cors());
+const corsOptions = {
+  origin: "http://127.0.0.1:9000" &&  "https://fitmantra.onrender.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json({ limit: "500mb" }));
 app.use(
   bodyParser.urlencoded({
@@ -57,13 +65,7 @@ app.get("/", (req, res) => {
 res.header("Access-Control-Allow-Origin","http://localhost:9000");
 });
 
-app.use(cors());
-const corsOptions = {
-  origin: "http://localhost:9000" &&  "https://fitmantra.onrender.com",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
