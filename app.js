334e7c029ab3a1a3f0beed2d247974b17750cc2b
onkarzende <onkarzende1997@gmail.com>
2023-12-20 14:23:25 +0530

    Post Functionality

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(
     limit: "500mb",
   })
 );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100'); // Replace with your frontend origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(cors());
 const corsOptions ={
   origin:'http://localhost:9000', 
